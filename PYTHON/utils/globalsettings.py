546763ada34dfc25efd8e3a95663a76d2551bb90
mcondedesimon <mcondedesimon@deloitte.es>
2023-08-04 13:46:54 +0200

    numpy sin lfs a ver si ahora

--- PYTHON/utils/globalsettings.py
+++ PYTHON/utils/globalsettings.py
@@ -19,6 +19,11 @@ the_files = prjSettings()
 
 the_files.ATTRITION = os.path.join(the_folders.DIR_DATA_RAW, "WA_Fn-UseC_-HR-Employee-Attrition.csv")
 the_files.ROBYN_DATA = os.path.join(the_folders.DIR_DATA_RAW, "dt_simulated_weekly.csv")
+the_files.DATA_LIGHTWEIGHT = os.path.join(the_folders.DIR_DATA_RAW, "lightweight_data.npz")
+the_files.TRAZE_LIGHTWEIGHT = os.path.join(the_folders.DIR_DATA_RAW, "lightweight_traze.npz")
+
+the_files.MEDIA_LONG = os.path.join(the_folders.DIR_DATA_CLEAN, "df_media_long.csv")
+the_files.EXTRA_LONG = os.path.join(the_folders.DIR_DATA_CLEAN, "df_extra_long.csv")
 
 the_files.CFG_FILE = os.path.join(the_folders.DIR_ROOT, "config.ini")
 
